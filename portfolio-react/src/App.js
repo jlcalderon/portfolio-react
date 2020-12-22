19c456929f10add9709eb4d52e4e4f7f51b44600
JLC <jlc@annonymous.com>
2020-12-22 06:56:46 -0600

    Adding footer component and css touches

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
     return (
         <div>
             <Header />
+            <div className="wrapper" style={{minHeight: "100vh"}}>
+
+            </div>
+            <Footer />
         </div>
     );
 }
